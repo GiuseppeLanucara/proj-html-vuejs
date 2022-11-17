2e51134ee1b4b07ae89760425d430b5ea9447d2d
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2022-11-17 16:02:27 +0100

    scaffholding components

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+    name: "AppHeader",
+
+    data() {
+        return {
+            links: [
+                {
+                    text: "Home",
+                    url: "#",
+                },
+                {
+                    text: "Courses",
+                    url: "#",
+                },
+                {
+                    text: "Istructor",
+                    url: "#",
+                },
+                {
+                    text: "Events",
+                    url: "#",
+                },
+                {
+                    text: "Pages",
+                    url: "#",
+                },
+                {
+                    text: "Elements",
+                    url: "#",
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>

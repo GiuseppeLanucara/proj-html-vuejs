65861e9708d0a29e7898de7b621bf396f96a4f15
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2022-11-18 15:53:31 +0100

    MainSecond component con struttura e array obj

---
+++ src/components/MainSecond.vue
@@ -0,0 +1,61 @@
+<script>
+export default {
+    name: 'MainSecond',
+    data() {
+        return {
+            informations: [
+                {
+                    titolo: "What We Do",
+                    status: true
+                },
+                {
+                    titolo: "Degree Programme",
+                    status: false
+                },
+                {
+                    titolo: "Career Achievements",
+                    status: false
+                },
+                {
+                    titolo: "Personal Managment",
+                    status: false
+                },
+                {
+                    titolo: "Steps To Success",
+                    status: false
+                },
+                {
+                    titolo: "Knowledge Transfer",
+                    status: false
+                },
+            ],
+            objectives: [
+                {
+                    titolo: "We enrich lives through learning.",
+                },
+                {
+                    titolo: "Maximizing potential through individual attention.",
+                },
+                {
+                    titolo: "The trusted name for specialized training.",
+                },
+                {
+                    titolo: "People thach. People learn. This is where they connect.",
+                },
+            ]
+        }
+    }
+}
+</script>
+
+
+<template>
+
+
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>

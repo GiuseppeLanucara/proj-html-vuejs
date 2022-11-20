e528e57b4550c9cbc1c7b8183c4bada5c3a96f95
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2022-11-20 17:14:01 +0100

    fix

--- src/data/card.js
+++ src/data/card.js
@@ -4,23 +4,23 @@ export default [
         professione: 'Android Developer',
         nome: 'David Sanders',
         articolo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat repellat libero molestiae obcaecati consequuntur laborum voluptatum, similique ipsum!',
-        titolo: "Free",
+        prezzo: "Free",
         status: true
     },
     {
         img: "course-6-f-img.jpg",
         professione: 'Web Designing',
         nome: 'Jennifer Powell',
-        articolo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat repellat libero molestiae obcaecati consequuntur laborum voluptatum, similique ipsum!',
-        titolo: "Free",
+        paragrafo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat repellat libero molestiae obcaecati consequuntur laborum voluptatum, similique ipsum!',
+        prezzo: "Free",
         status: true
     },
     {
         img: "blog-post-2.jpg",
         professione: 'Financial Modeling',
         nome: 'Edward Bowman',
-        articolo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat repellat libero molestiae obcaecati consequuntur laborum voluptatum, similique ipsum!',
-        titolo: "$20",
+        paragrafo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat repellat libero molestiae obcaecati consequuntur laborum voluptatum, similique ipsum!',
+        prezzo: "$20",
         status: false
     },
 
